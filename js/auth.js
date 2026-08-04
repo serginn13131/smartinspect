978a7ddf0fd7ad62cacf11a2345e2b02b695bd85
serginn13131 <0001253036@senaimgaluno.com.br>
2026-08-04 14:31:45 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,60 @@
 
+async function verificarLogin(){
+
+
+const {data}=await banco.auth.getSession();
+
+
+
+if(!data.session){
+
+
+window.location.href="login.html";
+
+
+return;
+
+
+}
+
+
+
+const usuario = JSON.parse(
+
+localStorage.getItem("usuario")
+
+);
+
+
+
+if(!usuario){
+
+
+window.location.href="login.html";
+
+
+return;
+
+
+}
+
+
+
+}
+
+
+
+
+async function sair(){
+
+
+await banco.auth.signOut();
+
+
+localStorage.removeItem("usuario");
+
+
+window.location.href="login.html";
+
+
+}
