b3f33f2a8297aef07cbc0de87c85da82d7c685ac
serginn13131 <0001253036@senaimgaluno.com.br>
2026-08-04 17:42:54 -0300

    Update perguntar.js

--- api/perguntar.js
+++ api/perguntar.js
@@ -1,76 +1,9 @@
-import OpenAI from "openai";
+export default function handler(req, res) {
 
+res.status(200).json({
 
-export default async function handler(req, res) {
-
-try {
-
-
-const apiKey = process.env.OPENAI_API_KEY;
-
-
-if(!apiKey){
-
-return res.status(500).json({
-
-erro:"OPENAI_API_KEY não encontrada na Vercel"
+teste:"API SmartInspect funcionando"
 
 });
-
-}
-
-
-
-const openai = new OpenAI({
-
-apiKey: apiKey
-
-});
-
-
-
-const resposta = await openai.chat.completions.create({
-
-model:"gpt-4.1-mini",
-
-messages:[
-
-{
-
-role:"user",
-
-content:req.body.pergunta
-
-}
-
-]
-
-});
-
-
-
-return res.status(200).json({
-
-resposta: resposta.choices[0].message.content
-
-});
-
-
-
-}catch(error){
-
-
-console.log(error);
-
-
-return res.status(500).json({
-
-erro:error.message
-
-});
-
-
-}
-
 
 }
