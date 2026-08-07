787e7d526fe407570142265ee87394f8d5a85d9b
serginn13131 <0001253036@senaimgaluno.com.br>
2026-08-07 08:24:37 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,10 +1,13 @@
+// Configuração Supabase
 
 const SUPABASE_URL = "https://zuvvaggooddpblqhgsdb.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_A1D2QDRqfVOU24huakpyPg_WGWYTXeQ";
 
 
+// Conexão com Supabase
+
 const banco = window.supabase.createClient(
-SUPABASE_URL,
-SUPABASE_KEY
+    SUPABASE_URL,
+    SUPABASE_KEY
 );
